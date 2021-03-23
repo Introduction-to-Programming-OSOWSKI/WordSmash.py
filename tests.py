014e9911241a31b3025b7e380c9558b3286071bd
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2021-03-23 09:09:05 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2020
-month = 12
-day = 17
+year = 2021
+month = 3
+day = 23
 
 def test_code():
     assert main.wordSmash("Cat", "Dog") == "CatDog", "Cat + Dog == CatDog failed"
